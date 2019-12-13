c7f9a94cf482ef3baa592f032a5c0217c79e612a
Sysnetics <fkholmatov@gmail.com>
2019-12-13 02:04:11 -0600

    Change of favicons, included PWA splash (launch) screens, and minified PNG images

--- components/PageLayout.js
+++ components/PageLayout.js
@@ -12,17 +12,41 @@ class PageLayout extends React.Component {
         <Head>
           <title>{this.props.page_title}</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-          <meta name="msapplication-TileColor" content="#00aba9" />
-          <meta name="theme-color" content="#ffffff" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
           <link rel="manifest" href="/site.webmanifest" />
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#180241" />
+
+          {/* Icons */}
+          <link rel="shortcut icon" href="favicon.ico" />
+          <link rel="icon" type="image/png" sizes="32x32" href="images/icons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="images/icons/favicon-16x16.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="images/icons/apple-touch-icon.png" />
+          <link rel="mask-icon" href="images/icons/safari-pinned-tab.svg" color="#111d5e" />
+
+          {/* PWA theme colors */}
+          <meta name="msapplication-TileColor" content="#111d5e" />
+          <meta name="theme-color" content="#ffffff" />
+
+          {/* PWA Splash screens for iOS devices */}
+          <link rel="apple-touch-startup-image" href="images/splash/launch-640x1136.png"
+            media="(device-width: 320px) and (device-height: 568px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-750x1334.png"
+            media="(device-width: 375px) and (device-height: 667px) and (-webkit-device-pixel-ratio: 2) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-1125x2436.png"
+            media="(device-width: 375px) and (device-height: 812px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-1242x2208.png"
+            media="(device-width: 414px) and (device-height: 736px) and (-webkit-device-pixel-ratio: 3) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-1536x2048.png"
+            media="(min-device-width: 768px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-1668x2224.png"
+            media="(min-device-width: 834px) and (max-device-width: 834px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
+          <link rel="apple-touch-startup-image" href="images/splash/launch-2048x2732.png"
+            media="(min-device-width: 1024px) and (max-device-width: 1024px) and (-webkit-min-device-pixel-ratio: 2) and (orientation: portrait)" />
         </Head>
+
         <NavBar />
         {this.props.children}
         <Footer />
+
       </>
     )
   }
